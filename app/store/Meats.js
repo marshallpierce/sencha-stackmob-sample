40f0640c2a7c6ae907e3a900e7fceed74e6b5a18
Marshall Pierce <marshall@palominolabs.com>
2012-06-20 19:23:16 -0700

    basic stackmob (pre oauth 2)

--- app/store/Meats.js
+++ app/store/Meats.js
@@ -1,13 +1,15 @@
 Ext.define('StackMobDemo.store.Meats', {
     extend: 'Ext.data.Store',
 
+    requires: ['StackMobDemo.data.proxy.StackMob'],
+
     config: {
         model: 'StackMobDemo.model.Meat',
         autoLoad: true,
         sorters: 'name',
         proxy: {
-            type: 'rest',
-            url: 'meats.json'
+            type: 'stackmob',
+            url: 'http://127.0.0.1:4567/meat'
         }
     }
 });
